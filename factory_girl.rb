d493f9aa09eee54e6ab646a65bae917241bb0bc3
Blake Johnson <blake41@@gmail.com>
2012-12-13 10:58:07 -0500

    spaces

--- factory_girl.rb
+++ factory_girl.rb
@@ -2,85 +2,85 @@ require 'debugger'
 
 module FactoryGirl
 
-	def self.configuration
-		@configuration ||= Configuration.new
-	end
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
 
-	def self.define(&block)
-		cr = CleanRoom.new
-		cr.instance_eval(&block)
-	end
+  def self.define(&block)
+    cr = CleanRoom.new
+    cr.instance_eval(&block)
+  end
 
-	def self.register_factory(factory)
-		factories[factory.name.to_sym] = factory 
-	end
+  def self.register_factory(factory)
+    factories[factory.name.to_sym] = factory 
+  end
 
-	def self.factories
-		configuration.factories
-	end
+  def self.factories
+    configuration.factories
+  end
 
-	def self.create(name)
-		factories[name]
-	end
+  def self.create(name)
+    factories[name]
+  end
 
 end
 
 class Factory
 
-	attr_accessor :name
+  attr_accessor :name
 
-	def initialize(name)
-		@name = name
-	end
+  def initialize(name)
+    @name = name
+  end
 
-	def declare_method(attr_name, value, proc)
-		set_value(attr_name, value, proc)
-		self.class.class_eval do
-			define_method("#{attr_name}") do
-				instance_variable_get("@#{attr_name}").send(:call)
-			end
-		end
-	end
+  def declare_method(attr_name, value, proc)
+    set_value(attr_name, value, proc)
+    self.class.class_eval do
+      define_method("#{attr_name}") do
+        instance_variable_get("@#{attr_name}").send(:call)
+      end
+    end
+  end
 
-	def set_value(attr_name, value, proc)
-		if proc.is_a?(Proc) && value.nil?
-			instance_variable_set("@#{attr_name}".to_sym, proc)
-		else
-			instance_variable_set("@#{attr_name}".to_sym, Proc.new {value})
-		end
-	end
+  def set_value(attr_name, value, proc)
+    if proc.is_a?(Proc) && value.nil?
+      instance_variable_set("@#{attr_name}".to_sym, proc)
+    else
+      instance_variable_set("@#{attr_name}".to_sym, Proc.new {value})
+    end
+  end
 
 end
 
 class CleanRoom
-	
-	def factory(name, &block)
-		@factory = Factory.new(name)
-		block.call
-		FactoryGirl.register_factory(@factory)
-	end
+  
+  def factory(name, &block)
+    @factory = Factory.new(name)
+    block.call
+    FactoryGirl.register_factory(@factory)
+  end
 
-	def method_missing(method, *args, &block)
-		@factory.declare_method(method, args[0], block)
-	end
+  def method_missing(method, *args, &block)
+    @factory.declare_method(method, args[0], block)
+  end
 end
 
 class Configuration
 
-	attr_accessor :factories
+  attr_accessor :factories
 
-	def initialize
-		@factories = {}
-	end
+  def initialize
+    @factories = {}
+  end
 
 end
 
 FactoryGirl.define do 
-	factory :user do
-		first_name "blake"
-		last_name "johnson"
-		height { 5.to_i }
-	end
+  factory :user do
+    first_name "blake"
+    last_name "johnson"
+    height { 5.to_i }
+  end
 end
 
 user = FactoryGirl.create(:user)
--- readme.md
+++ readme.md
@@ -1,10 +1,10 @@
 Implementing an api like so:
 ```ruby
 FactoryGirl.define do 
-	factory :user do
-		first_name "blake"
-		last_name "johnson"
-	end
+  factory :user do
+    first_name "blake"
+    last_name "johnson"
+  end
 end
 
 user = FactoryGirl.create(:user)
